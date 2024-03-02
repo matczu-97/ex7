fd070bbb81a0164fab1a633fcff6a840318d132c
Matan <cz.matan2@gmail.com>
2024-03-02 16:39:32 +0200

    another chagnge

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let app = angular.module('MyApp', []);
 app.controller('PhotoController', function() {
-    this.photos = Array.from({length: 20}, (_, index) => ({src: `https://picsum.photos/200/300?random=${index + 1}`, big: false}));
+    this.photos = Array.from({length: 10}, (_, index) => ({src: `https://picsum.photos/200/300?random=${index + 1}`, big: false}));
 
     this.photo = null;
 
