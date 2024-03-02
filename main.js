1c04c9193ab9047e1b97093607b83523948d0929
Matan <cz.matan2@gmail.com>
2024-03-02 16:29:17 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let app = angular.module('MyApp', []);
+app.controller('PhotoController', function() {
+    this.photos = Array.from({length: 42}, (_, index) => ({src: `https://picsum.photos/200/300?random=${index + 1}`, big: false}));
+
+    this.photo = null;
+
+    this.selected = function(photo) {
+        
+        this.photo = photo;
+    };
+});
